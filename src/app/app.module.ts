1734d897370e97e107ba974b6fa9d07c548f2781
unknown <zdravko.dugi@gmail.com>
2020-04-22 19:06:25 +0200

    fixed image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatDialogModule } from '@angular/material/dialog';
-
 import { ForecastGraphComponent } from './forecast-graph/forecast-graph.component'
 import { ForecastTableComponent } from './forecast-table/forecast-table.component'
-
 import { ChartsModule } from 'ng2-charts';
 import { ManagerDialogComponent } from './manager-dialog/manager-dialog.component';
 import { LocationInputComponent } from './location-input/location-input.component';
-
-
 import {MatChipsModule} from '@angular/material/chips';
-
-
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatIconModule} from '@angular/material/icon';
-
 import {MatSelectModule} from '@angular/material/select';
-
 import {MatTableModule} from '@angular/material/table'; 
-
 import {MatCardModule} from '@angular/material/card';
-
 import { HttpClientModule } from '@angular/common/http';
 import { DetailsDialogComponent } from './details-dialog/details-dialog.component'
-
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatTooltipModule} from '@angular/material/tooltip'; 
 
